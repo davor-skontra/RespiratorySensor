7eabb6a641e7e2b810f59aedc22639c979b97a95
Davor Skontra <thekrad23@gmail.com>
2020-03-27 20:22:10 +0200

    Start adding logic to ProgressiveTextViewModel

--- Assets/_Project/Actors/ProgressiveText/ProgressiveTextViewModel.cs
+++ Assets/_Project/Actors/ProgressiveText/ProgressiveTextViewModel.cs
@@ -1,12 +1,36 @@
 using System;
+using Actors.ProgressiveText;
 using UnityEngine;
 
 namespace ViewModels
 {
-    public class ProgressiveTextViewModel
+    public class ProgressiveTextViewModel: IDisposable
     {
+        private readonly ProgressiveTextModel _model;
+
+        private bool _initialized;
         public event Action<string> TextEvent;
         public event Action<Color> TextColorEvent;
         public event Action<bool> TextIsEnabledEvent;
+
+        public ProgressiveTextViewModel(ProgressiveTextModel model)
+        {
+            _model = model;
+
+            _model.ChangeScreenEvent += OnNextScreen;
+        }
+
+        private void OnNextScreen(ProgressiveScreen screen)
+        {
+            if (_initialized)
+            {
+                
+            }
+        }
+
+        public void Dispose()
+        {
+            _model.ChangeScreenEvent -= OnNextScreen;
+        }
     }
 }
